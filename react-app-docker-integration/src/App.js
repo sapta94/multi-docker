5ddada85a09cdbe0cfe1c27045b47308b2c0a646
sapta94 <dey7.kol@gmail.com>
2019-12-01 12:56:42 +0530

    chnages for react routing

--- react-app-docker-integration/src/App.js
+++ react-app-docker-integration/src/App.js
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarMenu from "./components/NavbarMenu"
 import Home from "./components/Home"
+import {BrowserRouter,Route} from 'react-router-dom'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-          <NavbarMenu/>
-          <Home/>
+        <BrowserRouter>
+          <div>
+            <NavbarMenu/>
+            <Route exact path="/" component={Home} />
+          </div> 
+        </BrowserRouter>
     </div>
   );
 }
--- react-app-docker-integration/src/components/NavbarMenu.js
+++ react-app-docker-integration/src/components/NavbarMenu.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import {Navbar,Nav,Button} from 'react-bootstrap';
+import {Link} from "react-router-dom"
 
 export default class NavbarMenu extends Component {
     render(){
@@ -8,9 +9,9 @@ export default class NavbarMenu extends Component {
             <Navbar bg="light" variant="light">
                 <Navbar.Brand href="#home">Pollution</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#home"><Link to='/'>Home</Link></Nav.Link>
                     <Nav.Link href="#features">Recents</Nav.Link>
-                    <Nav.Link href="#pricing">Settings</Nav.Link>
+                    <Nav.Link href="#settings">Settings</Nav.Link>
                 </Nav>
             </Navbar>
         )
