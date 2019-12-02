bd1314bd476d3e209f58c5d92b85bf443def7321
sapta94 <dey7.kol@gmail.com>
2019-12-02 22:58:24 +0530

    api integration

--- react-app-docker-integration/src/components/Recents.js
+++ react-app-docker-integration/src/components/Recents.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import {Card} from 'react-bootstrap'
 
 export default class Recents extends Component {
     constructor(props){
         super(props)
         this.state={
-
+            data:[]
         }
     }
     componentDidMount(){
         axios.get('/api/allRequests').then((resp)=>{
-            console.log(resp.data.data.rows)
+            this.setState({
+                data:resp.data.data.rows
+            })
         }).catch(err=>{
             console.log(err)
             alert('error in fetching data!')
@@ -18,7 +21,33 @@ export default class Recents extends Component {
     }
     render() {
         return (
-            <div> Recent Searches </div>
+            <div>
+                <h2> Recent Searches </h2>
+                <div className='row'>
+                    {
+                        (this.state.data).map((item)=>{
+                            return(
+                                <div style={{margin:'10px'}} className='col-md-3'>
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Body>
+                                            <Card.Title>{'Request #'+item.id}</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">{item.date_time}</Card.Subtitle>
+                                            <Card.Text>
+                                                {
+                                                    item.cities
+                                                }
+                                            </Card.Text>
+                                            <Card.Link href="#">See Results</Card.Link>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            )
+                            
+                        })
+                    }
+                </div>
+            </div>
+            
         );
     }
 }
